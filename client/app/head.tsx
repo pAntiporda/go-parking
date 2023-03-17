9c030cc8efea6fe42472c00bd11b2dff4ec5a297
Phil Antiporda <phil.antiporda@growthops.com.au>
2023-03-17 17:32:52 +1000

    chore: init linter. Integrate go linter config

--- client/app/head.tsx
+++ client/app/head.tsx
@@ -1,9 +1,13 @@
-export default function Head() {
-  return (
-    <>
-      <title></title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
-    </>
-  )
-}
+import {Fragment} from 'react';
+
+const Head = (): JSX.Element => {
+	return (
+		<Fragment>
+			<title/>
+			<meta content="width=device-width, initial-scale=1" name="viewport"/>
+			<link rel="icon" href="/favicon.ico"/>
+		</Fragment>
+	);
+};
+
+export default Head;
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,11 +1,11 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
-const RootLayout = ({children}: {children: React.ReactNode;}) => {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
+const RootLayout = ({children}: {children: React.ReactNode}): JSX.Element => {
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	);
+};
 
 export default RootLayout;
